2ef58dfd6319e5f1835ca383aa8fa30fe1689f72
mazen atef <mazen.atef@scandiweb.com>
2024-09-07 20:56:06 +0300

    task -made the required logic to get the countries data from the api -made the proper logic to add the countries retrieved into the database

--- src/Command/CountrySyncCommand.php
+++ src/Command/CountrySyncCommand.php
@@ -6,9 +6,19 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use App\Controller\DataFetcherController;
 
 class CountrySyncCommand extends Command
 {
+
+    private $controller;
+
+    public function __construct(DataFetcherController $controller)
+    {
+        parent::__construct();
+        $this->controller = $controller;
+    }
+
     protected function configure(): void
     {
         $this->setName('countries:sync');
@@ -17,6 +27,11 @@ class CountrySyncCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->controller->setContainer($this->getApplication()->getKernel()->getContainer());
+        $this->controller->fetchDataFromApi();
+
+        $output->writeln('finished');
+
         return COMMAND::SUCCESS;
     }
 }
