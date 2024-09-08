a765379c9f7050b1b6483685fb5ca6c8d38db282
mazen atef <mazen.atef@scandiweb.com>
2024-09-08 12:36:54 +0300

    task -made the required logic for the CRUD operations -made the necessary security procedures

--- src/Controller/V1/CountryController.php
+++ src/Controller/V1/CountryController.php
@@ -5,37 +5,109 @@ namespace App\Controller\V1;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CountryRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Country;
 
-#[Route('countries')]
+#[Route('/api/doc')]
 class CountryController extends AbstractController
 {
-    #[Route('/{country}', methods: ['GET'])]
-    public function getCountry(): void
-    {
-        // TODO
+    private $manager;
+    private $countryRepo;
+    private $serializer;
+
+    public function __construct(
+    CountryRepository $countryRepo,
+    EntityManagerInterface $manager,
+    SerializerInterface $serializer
+    ) {
+        $this->countryRepo = $countryRepo;
+        $this->manager = $manager;
+        $this->serializer = $serializer;
     }
 
-    #[Route('/list', methods: ['GET'])]
-    public function getCountries(): void
+    #[Route('/{uuid}', name:'getCountry', methods: ['GET'])]
+    public function getCountry(int $uuid): Response
     {
-        // TODO
+        $country = $this->countryRepo->find($uuid);
+
+        if (!$country) {
+            return new JsonResponse(['error' => 'Product not found'], 404);
+        }
+
+        $data = $this->serializer->serialize($country, 'json');
+
+        return new Response($data, 200, ['Content-Type' => 'application/json']);
     }
 
-    #[Route('/', methods: ['POST'])]
-    public function addCountry(): void
+    #[Route('/list', name:'getCountries',methods: ['GET'])]
+    public function getCountries(): Response
     {
-        // TODO
+        $countries = $this->countryRepo->findAll();
+        $data = $this->serializer->serialize($countries, 'json');
+
+       return new Response($data, 200, ['Content-Type' => 'application/json']);
     }
 
-    #[Route('/{country}', methods: ['PATCH'])]
-    public function updateCountry(): void
+    #[Route('/', name:'addCountry',methods: ['POST'])]
+    public function addCountry(Request $request): Response
     {
-        // TODO
+        $requestData = $request->getContent();
+
+       $country = $this->serializer->deserialize($requestData, Country::class, 'json');
+
+       if (
+        !$country->getUuid()||
+        !$country->getRegion()||
+        !$country->getSubRegion()||
+        !$country->getDemonym()||
+        !$country->getPopulation()||
+        !$country->isIndependant()||
+        !$country->getFlag()||
+        !$country->getCurrency()
+        ) {
+           return new JsonResponse(['error' => 'Missing required fields'], 400);
+       }
+
+       $this->manager->persist($country);
+       $this->manager->flush();
+
+       $data = $this->serializer->serialize($country, 'json');
+
+       return new JsonResponse(['message' => 'Country created!', 'country' => json_decode($data)], 201);
     }
 
-    #[Route('/{country}', methods: ['DELETE'])]
-    public function deleteCountry(): void
+    #[Route('/{uuid}', name:'updateCountry',methods: ['PATCH'])]
+    public function updateCountry(Country $country,Request $request): Response
     {
-        // TODO
+        $requestData = $request->getContent();
+       $updatedCountry = $serializer->deserialize($requestData, Country::class, 'json');
+
+       $$country->setUuid($updatedCountry->getUuid());
+       $$country->setName($updatedCountry->getName());
+       $$country->setRegion($updatedCountry->getRegion());
+       $$country->setSubRegion($updatedCountry->getSubRegion());
+       $$country->setDemonym($updatedCountry->getDemonym());
+       $$country->setPopulation($updatedCountry->getPopulation());
+       $$country->setIndependant($updatedCountry->isIndependant());
+       $$country->setFlag($updatedCountry->getFlag());
+       $$country->setCurrency($updatedCountry->getCurrency());
+
+       $this->manager->flush();
+
+       return new Response('Country updated!', 200);
+    }
+
+    #[Route('/{uuid}', name:'deleteCountry',methods: ['DELETE'])]
+    public function deleteCountry(Country $country): Response
+    {
+        $this->manager->remove($country);
+        $this->manager->flush();
+
+       return new Response('Country deleted!', 200);
     }
 }
